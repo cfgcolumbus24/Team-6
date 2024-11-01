5354519ca633b2cb66a7f2bb435d64f04280d510
thotakurk <keerthanathotakura@gmail.com>
2024-11-01 18:30:50 -0400

    Added MongoDB Connectivity and Functionality

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connectDB = require('./config/db'); // Import database connection function
+require('dotenv').config(); // Load environment variables from .env
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware to parse JSON data
+app.use(express.json());
+
+// Define a test route
+app.get('/', (req, res) => res.send('API is running'));
+
+// Define the port and start the server
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
