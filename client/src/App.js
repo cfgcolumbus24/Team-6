d51bd7706284c07c62d6a75486df3a7e6320a2f5
Jamaladen1 <168682048+Jamaladen1@users.noreply.github.com>
2024-11-01 23:03:10 -0400

    changed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 // src/App.js
 import React from 'react';
 import Header from './Components/Header';
+<<<<<<< HEAD
 import Body from './Components/Body';
 import Footer from './Components/Footer';
 import './App.css';
+=======
+import Footer from './Components/Footer';
+>>>>>>> 984e175 (footer)
 
 function App() {
   return (
@@ -11,9 +15,16 @@ function App() {
       {/* Header */}
       <Header />
 
+<<<<<<< HEAD
       {/* Main Content - Body */}
       <main className="main-content">
         <Body />
+=======
+      {/* Placeholder Main Content */}
+      <main className="main-content">
+        <h1>Welcome to Our Application</h1>
+        <p>This is the main content area. More pages will be added here later.</p>
+>>>>>>> 984e175 (footer)
       </main>
 
       {/* Footer */}
@@ -23,3 +34,8 @@ function App() {
 }
 
 export default App;
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> 984e175 (footer)
