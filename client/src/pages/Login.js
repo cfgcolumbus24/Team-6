6fd6c65b2a224722066b608f8221b64f509d1512
abhiram2504 <abhiram.rustagi@gmail.com>
2024-11-02 02:09:46 -0400

    yooo user auth finally works

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:5001/api/auth/login', formData);
+            const response = await axios.post('http://localhost:5001/api/login', formData);
             localStorage.setItem("token", response.data.token);
             alert("Login successful!");
             navigate('/home');
