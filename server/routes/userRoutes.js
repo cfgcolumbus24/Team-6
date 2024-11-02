9d484a85a51ae6ac53df0df540099fc92c504914
abhiram2504 <abhiram.rustagi@gmail.com>
2024-11-02 03:53:42 -0400

    changed userRoutes to have profile

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,15 +2,26 @@
 const express = require('express');
 const { login, register } = require('../controllers/userController');
 const { createUser, getAllUsers } = require('../controllers/userController');
-
+const User = require('../models/User');
 const router = express.Router();
 
-// Route to create a new user
+
 router.post('/', createUser);
 router.post('/register', register);
 router.post('/login', login);
 
 // Route to get all users
 router.get('/', getAllUsers);
+
+router.get('/profile/:username', async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.params.username });
+        if (!user) return res.status(404).send('User not found');
+        res.json(user); // Return the user data
+    } catch (error) {
+        res.status(500).send('Server error');
+    }
+});
+
 
 module.exports = router;
